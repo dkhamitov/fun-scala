ba75f41bbcfcb69e56deb400435751adcd77eebb
Dmitrii Kahmitov <khamitov.dm@gmail.com>
2016-12-15 00:21:11 +0300

    Initial commit

---
+++ src/main/scala-2.11/d/kh/scala/Book.scala
@@ -0,0 +1,6 @@
+package d.kh.scala
+
+/**
+  * Created by khamitovdm on 15/12/2016.
+  */
+case class Book(title: String, authors: List[String])
---
+++ src/test/scala-2.11/d/kh/scala/BookTest.scala
@@ -0,0 +1,8 @@
+package d.kh.scala
+
+/**
+  * Created by khamitovdm on 15/12/2016.
+  */
+class BookTest extends org.scalatest.FunSuite {
+
+}
