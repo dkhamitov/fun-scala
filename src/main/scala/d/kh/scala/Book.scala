3612cf62448908b383f5baba86c6cd643cfe4d00
Dmitrii Kahmitov <khamitov.dm@gmail.com>
2017-01-08 16:07:07 +0300

    findAllAuthorsWith2Books with a test added

--- src/main/scala/d/kh/scala/Book.scala
+++ src/main/scala/d/kh/scala/Book.scala
@@ -4,3 +4,21 @@ package d.kh.scala
   * Created by khamitovdm on 15/12/2016.
   */
 case class Book(title: String, authors: List[String])
+
+object Book {
+  /**
+    * @param books must meet the requirement that any author may appear at most 2 times. Otherwise, the behaviour
+    *              of the method is undefined
+    * @return
+    */
+  def findAllAuthorsWith2Books(books: List[Book]): List[String] = {
+    for {
+      b1 <- books
+      b2 <- books
+      if b1.title < b2.title
+      a1 <- b1.authors
+      a2 <- b2.authors
+      if a1 == a2
+    } yield a1
+  }
+}
--- src/test/scala/d/kh/scala/BookTest.scala
+++ src/test/scala/d/kh/scala/BookTest.scala
@@ -15,4 +15,14 @@ class BookTest extends FunSuite {
 
     assert(List(book1) == (for (b <- books; a <- b.authors if a startsWith "Joshua") yield b))
   }
+
+  test("find all the authors who has written more than 2 books") {
+    val book1 = Book(title = "Effective Java", authors = List("Joshua Bloch"))
+    val book2 = Book(title = "Concurrency in Practise", authors = List("Brian Goetz"))
+    val book3 = Book(title = "Java Puzzlers", authors = List("Joshua Bloch"))
+
+    val books = List(book1, book2, book3)
+
+    assert(List("Joshua Bloch") == Book.findAllAuthorsWith2Books(books))
+  }
 }
