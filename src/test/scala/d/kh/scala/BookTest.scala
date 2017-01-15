58f4b8e19907c3998adcd840a06b92253e6cbfec
Dmitrii Kahmitov <khamitov.dm@gmail.com>
2017-01-16 01:29:53 +0300

    Translate for-expression into high-order functions

--- src/test/scala/d/kh/scala/BookTest.scala
+++ src/test/scala/d/kh/scala/BookTest.scala
@@ -6,13 +6,22 @@ import org.scalatest.FunSuite
   * Created by khamitovdm on 15/12/2016.
   */
 class BookTest extends FunSuite {
-  test("just iterate through multiple books and find a particular author") {
+  test("just iterate through multiple books and find a particular author using a for-expression") {
     val book1 = Book(title = "Effective Java", authors = List("Joshua Bloch"))
     val book2 = Book(title = "Concurrency in Practise", authors = List("Brian Goetz"))
 
     val books = List(book1, book2)
 
-    assert(List(book1) == (for (b <- books; a <- b.authors if a startsWith "Joshua") yield b))
+    assert(List(book1.title) === (for (b <- books; a <- b.authors if a startsWith "Joshua") yield b.title))
+  }
+
+  test("just iterate through multiple books and find a particular author using high-order functions") {
+    val book1 = Book(title = "Effective Java", authors = List("Joshua Bloch"))
+    val book2 = Book(title = "Concurrency in Practise", authors = List("Brian Goetz"))
+
+    val books = List(book1, book2)
+
+    assert(List(book1.title) === books.flatMap(b => b.authors withFilter (a => a startsWith "Joshua") map (_ => b.title)))
   }
 
   test("find all the authors who has written at least 2 books") {
